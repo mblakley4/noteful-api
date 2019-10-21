edf275ffef7b22e5091ddaf20d430a36c5304ea2
Mike Blakley <m.blakley@icloud.com>
2019-10-21 11:56:30 -0400

    DB Tables and Migrations created with initial seed data

---
+++ README.md
@@ -0,0 +1,3 @@
+# Noteful API
+
+Server-side application supporting front end React App Noteful
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,4 @@
+INSERT INTO noteful_notes (name, content, folderId)
+VALUES
+  ('Welcome!', 'Hello Noteful users', 1),
+  ('My Notes', 'Create and update your notes and store them in folders.', 2);
